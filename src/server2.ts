53e7cf2f27b84f7db9b20f8ef490ccd01fb57a08
XStarsssssss <shinehtetaung30507@gmail.com>
2024-10-27 21:40:57 +0630

    init

--- src/server2.ts
+++ src/server2.ts
@@ -2,14 +2,14 @@ import express from 'express';
 const app = express();
 const port = 3001;
 
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
     res.send('Welcome World');
 });
 
-app.get('/about',(req,res)=> {
+app.get('/about', (req, res) => {
     res.send('About');
 });
 
-app.listen(port, () =>{
-     console.log(`Example app listening on port ${port}`);
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
